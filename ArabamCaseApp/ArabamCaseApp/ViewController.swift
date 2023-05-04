866c1c570d8ee869454a620dfcb66c06563d52cd
yusuf can bircan <yusufcan.bircan@arabam.com>
2023-05-04 15:38:32 +0300

    first commit

---
+++ ArabamCaseApp/ArabamCaseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArabamCaseApp
+//
+//  Created by Yusuf Can Bircan on 4.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
